df3e0269c356f3fe461596c18e797bafb650cc4c
UnknownTiffany <chut01@student.psdr3.org>
2024-11-20 17:14:22 -0600

    Character Movement + Falling Apple

--- Game Here/save/main.gd
+++ Game Here/save/main.gd
@@ -1,11 +1,16 @@
-extends CharacterBody2D
+extends Node2D
+#@export var squid_object : PackedScene
 
-var speed = 200
-
-var motion = Vector2.ZERO
-					
-func _physics_process(delta):
-	if Input.is_action_pressed("left"):
-		motion = move_and_collide(Vector2(-speed * delta, 0))
-	if Input.is_action_pressed("right"):
-		motion = move_and_collide(Vector2(speed * delta, 0))
+func _ready() -> void:
+	var timer = Timer.new()
+	timer.wait_time = 1.7
+	timer.autostart = true
+	add_child(timer)
+	timer.timeout.connect(_on_timer_timeout)
+	
+func _on_timer_timeout():
+	randomize()
+	var apple = preload("res://save/Apple.tscn").instantiate()
+	apple.position.y = 30
+	apple.position.x = randf_range(-470, 510)
+	add_child(apple)
