9e9ac9c88b246ad8ed520f92813e112c7c4181f9
UnknownTiffany <chut01@student.psdr3.org>
2025-03-13 11:45:09 -0500

    Main Menu + Instruction + restart button

--- Game Here/control.gd
+++ Game Here/control.gd
@@ -22,6 +22,7 @@ func _ready() -> void:
 		node.player.add_child(remote_transform)
 	
 	
+	
 	#var viewport1 = $VBoxContainer/SubViewportContainer/SubViewport
 	#var viewport2 = $VBoxContainer/SubViewportContainer2/SubViewport
 	#var camera1 = $VBoxContainer/SubViewportContainer/SubViewport/Player/Camera2D
